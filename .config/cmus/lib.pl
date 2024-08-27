e9f86498050862ea67ead06c608c57a3bd484e10
ThGreatExplorer <ThGreatExplorer@gmail.com>
2024-08-27 15:03:45 -0400

    updated dotfiles

--- .config/cmus/lib.pl
+++ .config/cmus/lib.pl
@@ -1,10 +1,13 @@
 /home/dyu/Music/50 Cent - In da Club (the video).mp3
+/home/dyu/Music/ABBA - Gimme! Gimme! Gimme! (A Man After Midnight).mp3
 /home/dyu/Music/Amy Winehouse - Back To Black.mp3
 /home/dyu/Music/Eminem - Lose Yourself.mp3
+/home/dyu/Music/Eurythmics - Sweet Dreams (Are Made of This) (remastered).mp3
 /home/dyu/Music/Juanes - La camisa negra.mp3
 /home/dyu/Music/Lady Gaga - Bad Romance.mp3
 /home/dyu/Music/Lady Gaga - Paparazzi.mp3
 /home/dyu/Music/Lana Del Rey - Young and Beautiful (DH orchestral version).mp3
+/home/dyu/Music/Lorde - Team.mp3
 /home/dyu/Music/SAINt JHN - Roses (Imanbek remix).mp3
 /home/dyu/Music/Taylor Swift - Love Story.mp3
 /home/dyu/Music/TERIYAKI BOYZ - TOKYO DRIFT (FAST & FURIOUS).mp3
